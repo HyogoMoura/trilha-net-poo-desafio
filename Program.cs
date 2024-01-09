4882725460bf43ccbafed86adfcbb39faf0b50c5
Diogo <diogofilipilsht@poli.br>
2024-01-08 23:16:38 -0300

    Commit de resolucao do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("88199327724"," G60", "001970-01-010000-0",256);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Iphone ip = new Iphone("998453377","Iphone 15", "201970-01-21045-9",128);
+ip.Ligar();
+ip.ReceberLigacao();
+ip.InstalarAplicativo("X");
